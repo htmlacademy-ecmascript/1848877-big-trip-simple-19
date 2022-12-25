c79b9a690f92894831fd9b255c5016a118c31c9b
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2022-12-25 22:34:19 +0700

    fix

--- src/utils/task.js
+++ src/utils/task.js
@@ -2,23 +2,13 @@ import dayjs from 'dayjs';
 
 const DATE_FORMAT = 'DD/MM/YY HH:mm';
 
-export function humanizePointDueDate(dueDate) {
-  return dueDate ? dayjs(dueDate).format(DATE_FORMAT) : '';
-}
+export const humanizePointDueDate = (dueDate) => dueDate ? dayjs(dueDate).format(DATE_FORMAT) : '';
 
-export function isPointBeforeOrSame(dueDate) {
-  return dueDate && dayjs().isSameOrBefore(dueDate, 'D');
-}
+export const isPointBeforeOrSame = (dueDate) => dueDate && dayjs().isSameOrBefore(dueDate, 'D');
 
-export function isPointSame(dueDate) {
-  return dueDate && dayjs().isSame(dueDate, 'D');
-}
+export const isPointSame = (dueDate) => dueDate && dayjs().isSame(dueDate, 'D');
 
-export function isPointAfter(dueDate) {
-  return dueDate && dayjs().isAfter(dueDate, 'D');
-}
+export const isPointAfter = (dueDate) => dueDate && dayjs().isAfter(dueDate, 'D');
 
-export function isPointBefore(dueDate) {
-  return dueDate && dayjs().isBefore(dueDate, 'D');
-}
+export const isPointBefore = (dueDate) => dueDate && dayjs().isBefore(dueDate, 'D');
 
