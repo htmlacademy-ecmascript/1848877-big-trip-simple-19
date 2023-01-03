ee423fe5ac7aa0d16320f284c16caea99fd6b266
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-01-03 18:39:34 +0700

    fix

--- src/utils/task.js
+++ src/utils/task.js
@@ -4,6 +4,6 @@ export const humanizeEventDate = (date) => dayjs(date).format('MMM DD');
 
 export const humanizeEventTime = (date) => dayjs(date).format('HH:mm');
 
-export const isFutureTask = (dateFrom, dateTo) => dayjs().isBefore(dayjs(dateFrom)) || dayjs().isSame(dayjs(dateFrom), 'day') || (dayjs().isAfter(dayjs(dateFrom)) && dayjs().isBefore(dayjs(dateTo)))
+export const isFutureTask = (dateFrom, dateTo) => dayjs().isBefore(dayjs(dateFrom)) || dayjs().isSame(dayjs(dateFrom), 'day') || (dayjs().isAfter(dayjs(dateFrom)) && dayjs().isBefore(dayjs(dateTo)));
 
 
