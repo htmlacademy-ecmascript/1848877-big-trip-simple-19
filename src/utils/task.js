e4366f45e2ba1b315461dde90ec1972cc6541046
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-01-04 21:44:06 +0700

    create sort func

--- src/utils/task.js
+++ src/utils/task.js
@@ -6,4 +6,31 @@ export const humanizeEventTime = (date) => dayjs(date).format('HH:mm');
 
 export const isFutureTask = (dateFrom, dateTo) => dayjs().isBefore(dayjs(dateFrom)) || dayjs().isSame(dayjs(dateFrom), 'day') || (dayjs().isAfter(dayjs(dateFrom)) && dayjs().isBefore(dayjs(dateTo)));
 
+export const getWeightForNullDate = (dateA, dateB) => {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
+
+  if (dateA === null) {
+    return 1;
+  }
+
+  if (dateB === null) {
+    return -1;
+  }
+
+  return null;
+};
+
+export const sortPointDate = (pointA, pointB) => {
+  const weight = getWeightForNullDate(pointA.dateFrom, pointB.dateFrom);
+
+  return weight ?? dayjs(pointA.dateFrom).diff(dayjs(pointB.dateFrom));
+};
+
+export const sortPointPrice = (pointA, pointB) => {
+  const weight = getWeightForNullDate(pointA.dateFrom, pointB.dateFrom);
+
+  return weight ?? pointB.basePrice - pointA.basePrice;
+};
 
