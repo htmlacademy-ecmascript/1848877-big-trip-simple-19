ce8693c323a07c68bf753d5049b1d1396449234c
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-01-18 22:53:56 +0700

    fix

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -5,7 +5,7 @@ function createSortTemplate(currentSortType) {
   return ( `
 <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
   <div class="trip-sort__item  trip-sort__item--day">
-    <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
+    <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
     <label class="trip-sort__btn" for="sort-day" ${currentSortType === SortType.DATE ? 'checked' : ''} data-sort-type="${SortType.DATE}">Day</label>
   </div>
 
@@ -20,7 +20,7 @@ function createSortTemplate(currentSortType) {
   </div>
 
   <div class="trip-sort__item  trip-sort__item--price">
-    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
+    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
     <label class="trip-sort__btn" for="sort-price"  ${currentSortType === SortType.PRICE ? 'checked' : ''} data-sort-type="${SortType.PRICE}">Price</label>
   </div>
 
