1fdca0209e7ea900bd6e9657aa84620e61eba6fe
Anna-Lyamytskih <annalyamytskih@yandex.ru>
2023-01-27 21:06:42 +0700

    fix section offers & destinations

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -149,15 +149,17 @@ function createPointEditTemplate(tripPoint, pointCommon) {
     </header>
     <section class="event__details">
       <section class="event__section  event__section--offers">
-        <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
+       ${!pointTypeOffer || !pointTypeOffer.offers || pointTypeOffer.offers.length === 0
+      ? ''
+      : '<h3 class="event__section-title  event__section-title--offers">Offers</h3>'}
         <div class="event__available-offers">
         ${createSectionOffersEditTemplate(pointTypeOffer, offers, type)}
         </div>
       </section>
 
       <section class="event__section  event__section--destination">
-        <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+       ${pointDestination ? '<h3 class="event__section-title  event__section-title--destination">Destination</h3>'
+      : '' }
         <p class="event__destination-description">${pointDestination ? pointDestination.description : ''}</p>
         <div class="event__photos-container">
           <div class="event__photos-tape">
